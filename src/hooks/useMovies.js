8cadb82b9e27d8c43679be2ce24d34014cce558e
Hazar NAMDAR <hazarnamdarrr@gmail.com>
2024-10-18 21:19:33 +0300

    fix(core): A new file directory and a file added. Old school movie fetch refactored with using a custom hook name: useMovies

---
+++ src/hooks/useMovies.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+
+export function useMovies(query, callback) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    callback?.();
+
+    const controller = new AbortController();
+
+    async function getMoviesByQuery(query) {
+      try {
+        setIsLoading(true);
+        setError('');
+
+        const res = await fetch(
+          `https://www.omdbapi.com/?&apikey=${process.env.REACT_APP_KEY}&s=${query}`,
+          {
+            signal: controller.signal,
+          }
+        );
+        const data = await res.json();
+
+        if (!res.ok) throw new Error('Failed to fetch!');
+
+        if (!data.Search) throw new Error(data.Error);
+
+        setMovies(data.Search);
+        setError('');
+      } catch (err) {
+        console.error(err);
+
+        if (err.name !== 'AbortError') setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    if (query.length < 3) {
+      setMovies([]);
+      setError('');
+      return;
+    }
+
+    getMoviesByQuery(query);
+
+    return () => controller.abort();
+  }, [query, callback]);
+
+  return { movies, isLoading, error };
+}
